050e9cae1e0a809823c14a3fecc14c803c798697
mcacherma <86954531+mcacherma@users.noreply.github.com>
2021-07-05 22:08:03 +0200

    Create R_code_multvar.r

---
+++ R_code_multvar.r
@@ -0,0 +1,25 @@
+install.packages("vegan")
+library(vegan)
+
+
+load("biomes_multivar.RData")
+ls()
+
+# plot per species matrix
+head(biomes)
+
+multivar <- decorana(biomes)
+multivar
+
+plot(multivar)
+
+# biomes names in the graph:
+attach(biomes_types)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T) 
+
+pdf("multivar.pdf")
+plot(multivar)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T) 
+dev.off()
