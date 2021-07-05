82c9284ea2f887bbd778d1a5d3bf1c9b12ac48d0
mcacherma <86954531+mcacherma@users.noreply.github.com>
2021-07-05 17:52:01 +0200

    Create R_code_pointpattern.r

---
+++ R_code_pointpattern.r
@@ -0,0 +1,100 @@
+# Point pattern analysis
+
+# If you did not install the spatstat package, do it by:
+# install.packages("spatstat")
+library(spatstat)
+
+######### IMPORT DATA FROM AN EXTERNAL SOURCE
+
+# setwd("path/lab")
+
+# setwd for Windows
+# setwd("C:/lab/")
+
+# setwd Mac
+# setwd("/Users/yourname/lab/")
+
+# Linux
+setwd("~/lab/")
+covid <- read.table("covid_agg.csv", header=TRUE)
+covid
+
+# Now, let's see the density of the covid data!
+# let's make a planar point pattern in spatstat
+attach(covid)
+# x, y, ranges
+covid_planar <- ppp(x, y, c(-180,180), c(-90,90))
+
+density_map <- density(covid_planar)
+
+plot(density_map)
+points(covid_planar)
+
+cl <- colorRampPalette(c('yellow','orange','red'))(100) # 
+plot(density_map, col = cl)
+points(covid_planar)
+
+# Exercise: change the colour of the map
+cl <- colorRampPalette(c('blue','yellow','orange','red','magenta'))(100) # 
+plot(density_map, col = cl)
+points(covid_planar)
+
+# Putting the coutnries ontop of the map
+
+# for Linux: 
+# sudo apt-get update
+# sudo apt-get install libgdal-dev libproj-dev
+
+# Install the rgdal package by:
+# install.packages("rgdal")
+
+library(rgdal)
+
+coastlines <- readOGR("ne_10m_coastline.shp")
+
+cl <- colorRampPalette(c('pink','green','orange','red','magenta'))(100) # 
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
+
+png("figure1.png")
+cl <- colorRampPalette(c('pink','green','orange','red','magenta'))(100) # 
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
+dev.off()
+
+pdf("figure1.pdf")
+cl <- colorRampPalette(c('pink','green','orange','red','magenta'))(100) # 
+plot(density_map, col = cl)
+points(covid_planar, pch = 19, cex = 0.5)
+plot(coastlines, add = TRUE)
+dev.off()
+
+###### interpolate case data
+
+marks(covid_planar) <- cases
+cases_map <- Smooth(covid_planar)
+
+plot(cases_map, col = cl)
+points(covid_planar)
+plot(coastlines, add = T)
+
+##### 
+# install sf package by:
+# install.packages("sf")
+library(sf)
+
+Spoints <- st_as_sf(covid, coords = c("lon", "lat"))
+
+cl <- colorRampPalette(c('lightpink2','lightsalmon','tomato1','red3','maroon'))(100)
+plot(cases_map, col = cl)
+plot(Spoints, cex=Spoints$cases/10000, col = 'purple3', lwd = 3, add=T)
+
+library(rgdal)
+# put a smoother to the coastlines by resampling
+coastlines <- readOGR("ne_10m_coastline.shp")
+plot(coastlines, add=T)
+
+
+
